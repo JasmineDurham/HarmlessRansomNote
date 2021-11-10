380d3c87505c02860956a83497cbb63f19a0fec3
Jasmine Durham <jasminedurham@jasmines-air.myfiosgateway.com>
2021-11-10 15:17:25 -0500

    still working

--- src/main/java/RansomNote.java
+++ src/main/java/RansomNote.java
@@ -1,5 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class RansomNote {
     public Boolean harmlessRansomNote(String message , String magazineText){
-        return null;
+
+        HashMap<String ,Integer> wordCount = new HashMap<>();
+         if(!wordCount.containsKey(message))
+             wordCount.put(message,1);
+         else
+             wordCount.put(message,wordCount.get(message)+1);
+       for( String :  wordCount.keySet()){
+            System.out.println("message" + message + "occurred" + wordCount.get(message)
+            + "times";
+        }
+
+
     }
 }
